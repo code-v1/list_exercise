58e24b1db90c0f8711084c2dbf3e20cbf3b11f65
Eric Tovar <tovareric@gmail.com>
2019-06-05 16:00:32 -0500

    for loop iterate

---
+++ exercise_three.py
@@ -0,0 +1,12 @@
+# use a for loop , print just last two food strings from 'foods'
+
+foods = (
+    'burgers',
+    'pickles',
+    'donuts',
+    'burritos'
+)
+
+
+for food in foods[-2:]:
+     print (food)

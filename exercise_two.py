490bc90e25125a9bff889617bcab8608b310d047
Eric Tovar <tovareric@gmail.com>
2019-06-05 16:00:01 -0500

    for loop usage

---
+++ exercise_two.py
@@ -0,0 +1,12 @@
+# create tuple named 'foods' 
+# use for loop to print out string 'food goes here is a good food'
+
+foods = {
+    'burgers',
+    'pickles',
+    'donuts',
+    'burritos'
+}
+
+for food in foods:
+    print(f'{food} is a good food!') 

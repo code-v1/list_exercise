b6a5295b52f5bf212b986a7f574d01efd6a55ad9
Eric Tovar <tovareric@gmail.com>
2019-06-05 15:59:20 -0500

    picking out items

---
+++ exercise_one.py
@@ -0,0 +1,14 @@
+#Create list named 'students'
+#print out second and last student name
+
+students = {
+    'student1':'berry',
+    'student2':'pickle',
+    'student3':'wilder',
+    'student4':'benny',
+    'student5':'doddy'
+
+}
+
+print ('Second student:', students.get('student2'))
+print ('Last student', students.get('student5'))

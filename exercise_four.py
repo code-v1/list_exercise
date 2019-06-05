176c3adce56472b85eb747713feed80313ff2b8c
Eric Tovar <tovareric@gmail.com>
2019-06-05 16:01:07 -0500

    create dict with keys

---
+++ exercise_four.py
@@ -0,0 +1,10 @@
+# Create a dictonary named home_town containing keys of city , state and population
+#print string with format 'I was born in city, state - population of population'
+
+home_town = {
+  'city': 'Dallas',
+  'state': 'Texas',
+  'population': "1,000,000"
+}
+
+print (f"I was born in {home_town['city']}, {home_town['state']} - population of {home_town['population']}")
